29fa6e50a964e24a9910de847c78e7ef43ede309
Brett Moore & Connor Abdelnoor <student@epicodus.com>
2015-03-18 11:32:56 -0700

    Create test for save method on Cuisine object and edit methods so test passes.

--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -44,7 +44,26 @@
                 $result = Cuisine::getAll();
 
                 //Assert
-                $this->assertEquals($test_Cuisine, $result[0]);    
+                $this->assertEquals($test_Cuisine, $result[0]);
+            }
+
+            function test_getAll()
+            {
+                //Arrange
+                $type = "AMERICAN";
+                $id = null;
+                $test_Cuisine = new Cuisine($type, $id);
+                $type2 = "Sicilian";
+                $test_Cuisine2 = new Cuisine($type, $id);
+
+                //Act
+                $test_Cuisine->save();
+                $test_Cuisine2->save();
+
+                $result = Cuisine::getAll();
+
+                //Assert
+                $this->assertEquals([$test_Cuisine, $test_Cuisine2], $result);
             }
 
 
